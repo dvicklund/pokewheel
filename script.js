61f7e0f17891d1ef7994ee4ad44dc9143f6e7699
David Vicklund <david@vicklund.com>
2016-04-14 14:38:51 -0700

    add bar chart with mock sales data

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ d3.csv('./data/types.csv', function(data) {
     typeArr.push({id: d.id, name: d.identifier, count: 0})
     if(i === data.length -1) {
       for(var i = 0; i < typeArr.length; i++) {
-        lookup[typeArr[i].id] = typeArr[i];
+        lookup[typeArr[i].id] = typeArr[i]
       }
     }
   })
@@ -41,8 +41,8 @@ d3.csv('./data/types.csv', function(data) {
 
 d3.csv('./data/pokemon_types.csv', function(data) {
   data.forEach(function(d, i, data) {
-    d.type_id = +d.type_id;
-    lookup[d.type_id].count++;
+    d.type_id = +d.type_id
+    lookup[d.type_id].count++
     if(i === data.length -1) {
       readPokemon();
     }
@@ -111,3 +111,89 @@ function readPokemon() {
     .attr('dy', '.35em')
     .text(function(d) {return d.data.name})
 }
+
+
+// Sales time - s'do this
+
+
+function readSales() {
+  var productArr = []
+  var productLookup = {};
+
+  var salesData = d3.json('./data/salesData.json', function(err, data) {
+    data.forEach(function(d, i, data) {
+      var formatCurr = Number(d.Quote.replace(/[^0-9\.]+/g, ''))
+      if(!productLookup.hasOwnProperty(d.Product)) {
+        productArr.push({product: d.Product, total: formatCurr})
+        productLookup[d.Product] = productArr[productArr.length -1]
+      } else {
+        productLookup[d.Product].total += formatCurr
+      }
+
+      if(i === data.length -1) {
+        makeSalesChart()
+      }
+    })
+
+    function makeSalesChart() {
+      var margin = {top: 20, right: 20, bottom: 120, left: 60}
+        , width = 1200 - margin.left - margin.right
+        , height = 1000 - margin.top - margin.bottom
+
+      var x = d3.scale.ordinal()
+          .rangeRoundBands([0, width], .1)
+
+      var y = d3.scale.linear()
+          .range([height, 0])
+
+      var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient('bottom')
+
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient('left')
+          .ticks(10, '$')
+
+      var svg = d3.select('body').append('svg')
+          .attr('width', width + margin.left + margin.right)
+          .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+      var data = productArr
+
+      x.domain(data.map(function(d) {return d.product}))
+      y.domain([0, d3.max(data, function(d) {return d.total})])
+
+      svg.append('g')
+          .attr('class', 'xAxis')
+          .attr('transform', 'translate(0,' + height + ')')
+          .call(xAxis)
+        .selectAll('text')
+          .attr('transform', 'rotate(90) translate(10,-15)')
+          .style('text-anchor', 'start')
+
+      svg.append('g')
+          .attr('class', 'yAxis')
+          .call(yAxis)
+        .append('text')
+          .attr('transform', 'rotate(-90)')
+          .attr('y', 6)
+          .attr('dy', '.71em')
+          .style('text-anchor', 'end')
+          .text('Total')
+
+      svg.selectAll('.bar')
+          .data(data)
+        .enter().append('rect')
+          .attr('class', 'bar')
+          .attr('x', function(d) {return x(d.product)})
+          .attr('width', x.rangeBand())
+          .attr('y', function(d) {return y(d.total)})
+          .attr('height', function(d) {return height - y(d.total)})
+    }
+  });
+}
+
+readSales()
